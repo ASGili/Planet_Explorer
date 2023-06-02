a038be6ba121538293bd519da3d08797b6c3e3bb
Angel <angelgonzc@yahoo.com>
2023-06-02 12:03:57 +0100

    mixor fixes on the seeds

--- asha/server/db/seeds.js
+++ asha/server/db/seeds.js
@@ -6,24 +6,16 @@ use planets;
    "Color": "Grey",
    "Mass (10^24kg)": 0.33,
    "Diameter (km)": 4879,
-   "Density (kg": {
-      "m^3)": 5429
-   },
-   "Surface Gravity(m": {
-      "s^2)": 3.7
-   },
-   "Escape Velocity (km": {
-      "s)": 4.3
-   },
+   "Density (kgm^3)": 5429,
+   "Surface Gravity(ms^2)": 3.7,
+   "Escape Velocity (kms)": 4.3,
    "Rotation Period (hours)": 1407.6,
    "Length of Day (hours)": 4222.6,
    "Distance from Sun (10^6 km)": 57.9,
    "Perihelion (10^6 km)": 46,
    "Aphelion (10^6 km)": 69.8,
    "Orbital Period (days)": "88",
-   "Orbital Velocity (km": {
-      "s)": 47.4
-   },
+   "Orbital Velocity (kms)": 47.4,
    "Orbital Inclination (degrees)": 7,
    "Orbital Eccentricity": 0.206,
    "Obliquity to Orbit (degrees)": 0.034,
@@ -38,24 +30,16 @@ use planets;
    "Color": "Brown and Grey",
    "Mass (10^24kg)": 4.87,
    "Diameter (km)": 12104,
-   "Density (kg": {
-      "m^3)": 5243
-   },
-   "Surface Gravity(m": {
-      "s^2)": 8.9
-   },
-   "Escape Velocity (km": {
-      "s)": 10.4
-   },
+   "Density (kgm^3)": 5243,
+   "Surface Gravity(ms^2)": 8.9,
+   "Escape Velocity (kms)": 10.4,
    "Rotation Period (hours)": -5832.5,
    "Length of Day (hours)": 2802,
    "Distance from Sun (10^6 km)": 108.2,
    "Perihelion (10^6 km)": 107.5,
    "Aphelion (10^6 km)": 108.9,
    "Orbital Period (days)": "224.7",
-   "Orbital Velocity (km": {
-      "s)": 35
-   },
+   "Orbital Velocity (kms)": 35,
    "Orbital Inclination (degrees)": 3.4,
    "Orbital Eccentricity": 0.007,
    "Obliquity to Orbit (degrees)": 177.4,
@@ -70,24 +54,16 @@ use planets;
    "Color": "Blue, Brown Green and White",
    "Mass (10^24kg)": 5.97,
    "Diameter (km)": 12756,
-   "Density (kg": {
-      "m^3)": 5514
-   },
-   "Surface Gravity(m": {
-      "s^2)": 9.8
-   },
-   "Escape Velocity (km": {
-      "s)": 11.2
-   },
+   "Density (kgm^3)": 5514,
+   "Surface Gravity(ms^2)": 9.8,
+   "Escape Velocity (kms)": 11.2,
    "Rotation Period (hours)": 23.9,
    "Length of Day (hours)": 24,
    "Distance from Sun (10^6 km)": 149.6,
    "Perihelion (10^6 km)": 147.1,
    "Aphelion (10^6 km)": 152.1,
    "Orbital Period (days)": "365.2",
-   "Orbital Velocity (km": {
-      "s)": 29.8
-   },
+   "Orbital Velocity (kms)": 29.8,
    "Orbital Inclination (degrees)": 0,
    "Orbital Eccentricity": 0.017,
    "Obliquity to Orbit (degrees)": 23.4,
@@ -102,24 +78,16 @@ use planets;
    "Color": "Red, Brown and Tan",
    "Mass (10^24kg)": 0.642,
    "Diameter (km)": 6792,
-   "Density (kg": {
-      "m^3)": 3934
-   },
-   "Surface Gravity(m": {
-      "s^2)": 3.7
-   },
-   "Escape Velocity (km": {
-      "s)": 5
-   },
+   "Density (kgm^3)": 3934,
+   "Surface Gravity(ms^2)": 3.7,
+   "Escape Velocity (kms)": 5,
    "Rotation Period (hours)": 24.6,
    "Length of Day (hours)": 24.7,
    "Distance from Sun (10^6 km)": 228,
    "Perihelion (10^6 km)": 206.7,
    "Aphelion (10^6 km)": 249.3,
    "Orbital Period (days)": "687",
-   "Orbital Velocity (km": {
-      "s)": 24.1
-   },
+   "Orbital Velocity (kms)": 24.1,
    "Orbital Inclination (degrees)": 1.8,
    "Orbital Eccentricity": 0.094,
    "Obliquity to Orbit (degrees)": 25.2,
@@ -134,24 +102,16 @@ use planets;
    "Color": "Brown, Orange and Tan, with White cloud stripes",
    "Mass (10^24kg)": 1898,
    "Diameter (km)": 142984,
-   "Density (kg": {
-      "m^3)": 1326
-   },
-   "Surface Gravity(m": {
-      "s^2)": 23.1
-   },
-   "Escape Velocity (km": {
-      "s)": 59.5
-   },
+   "Density (kgm^3)": 1326,
+   "Surface Gravity(ms^2)": 23.1,
+   "Escape Velocity (kms)": 59.5,
    "Rotation Period (hours)": 9.9,
    "Length of Day (hours)": 9.9,
    "Distance from Sun (10^6 km)": 778.5,
    "Perihelion (10^6 km)": 740.6,
    "Aphelion (10^6 km)": 816.4,
    "Orbital Period (days)": "4331",
-   "Orbital Velocity (km": {
-      "s)": 13.1
-   },
+   "Orbital Velocity (kms)": 13.1,
    "Orbital Inclination (degrees)": 1.3,
    "Orbital Eccentricity": 0.049,
    "Obliquity to Orbit (degrees)": 3.1,
@@ -166,24 +126,16 @@ use planets;
    "Color": "Golden, Brown, and Blue-Grey",
    "Mass (10^24kg)": 568,
    "Diameter (km)": 120536,
-   "Density (kg": {
-      "m^3)": 687
-   },
-   "Surface Gravity(m": {
-      "s^2)": 9
-   },
-   "Escape Velocity (km": {
-      "s)": 35.5
-   },
+   "Density (kgm^3)": 687,
+   "Surface Gravity(ms^2)": 9,
+   "Escape Velocity (kms)": 35.5,
    "Rotation Period (hours)": 10.7,
    "Length of Day (hours)": 10.7,
    "Distance from Sun (10^6 km)": 1432,
    "Perihelion (10^6 km)": 1357.6,
    "Aphelion (10^6 km)": 1506.5,
    "Orbital Period (days)": "10,747",
-   "Orbital Velocity (km": {
-      "s)": 9.7
-   },
+   "Orbital Velocity (kms)": 9.7,
    "Orbital Inclination (degrees)": 2.5,
    "Orbital Eccentricity": 0.052,
    "Obliquity to Orbit (degrees)": 26.7,
@@ -230,24 +182,16 @@ use planets;
    "Color": "Blue",
    "Mass (10^24kg)": 102,
    "Diameter (km)": 49528,
-   "Density (kg": {
-      "m^3)": 1638
-   },
-   "Surface Gravity(m": {
-      "s^2)": 11
-   },
-   "Escape Velocity (km": {
-      "s)": 23.5
-   },
+   "Density (kgm^3)": 1638,
+   "Surface Gravity(ms^2)": 11,
+   "Escape Velocity (kms)": 23.5,
    "Rotation Period (hours)": 16.1,
    "Length of Day (hours)": 16.1,
    "Distance from Sun (10^6 km)": 4515,
    "Perihelion (10^6 km)": 4471.1,
    "Aphelion (10^6 km)": 4558.9,
    "Orbital Period (days)": "59,800",
-   "Orbital Velocity (km": {
-      "s)": 5.4
-   },
+   "Orbital Velocity (kms)": 5.4,
    "Orbital Inclination (degrees)": 1.8,
    "Orbital Eccentricity": 0.01,
    "Obliquity to Orbit (degrees)": 28.3,
