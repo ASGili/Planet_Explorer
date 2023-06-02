2e39a0965ccb5f9402f5f4903383c9390b59912c
Angel <angelgonzc@yahoo.com>
2023-06-02 11:10:00 +0100

    planting seeds

---
+++ asha/server/db/seeds.js
@@ -0,0 +1,260 @@
+use planets;
+
+[
+ {
+   "Planet": "Mercury",
+   "Color": "Grey",
+   "Mass (10^24kg)": 0.33,
+   "Diameter (km)": 4879,
+   "Density (kg": {
+      "m^3)": 5429
+   },
+   "Surface Gravity(m": {
+      "s^2)": 3.7
+   },
+   "Escape Velocity (km": {
+      "s)": 4.3
+   },
+   "Rotation Period (hours)": 1407.6,
+   "Length of Day (hours)": 4222.6,
+   "Distance from Sun (10^6 km)": 57.9,
+   "Perihelion (10^6 km)": 46,
+   "Aphelion (10^6 km)": 69.8,
+   "Orbital Period (days)": "88",
+   "Orbital Velocity (km": {
+      "s)": 47.4
+   },
+   "Orbital Inclination (degrees)": 7,
+   "Orbital Eccentricity": 0.206,
+   "Obliquity to Orbit (degrees)": 0.034,
+   "Mean Temperature (C)": 167,
+   "Surface Pressure (bars)": "0",
+   "Number of Moons": 0,
+   "Ring System?": "No",
+   "Global Magnetic Field?": "Yes"
+},
+ {
+   "Planet": "Venus",
+   "Color": "Brown and Grey",
+   "Mass (10^24kg)": 4.87,
+   "Diameter (km)": 12104,
+   "Density (kg": {
+      "m^3)": 5243
+   },
+   "Surface Gravity(m": {
+      "s^2)": 8.9
+   },
+   "Escape Velocity (km": {
+      "s)": 10.4
+   },
+   "Rotation Period (hours)": -5832.5,
+   "Length of Day (hours)": 2802,
+   "Distance from Sun (10^6 km)": 108.2,
+   "Perihelion (10^6 km)": 107.5,
+   "Aphelion (10^6 km)": 108.9,
+   "Orbital Period (days)": "224.7",
+   "Orbital Velocity (km": {
+      "s)": 35
+   },
+   "Orbital Inclination (degrees)": 3.4,
+   "Orbital Eccentricity": 0.007,
+   "Obliquity to Orbit (degrees)": 177.4,
+   "Mean Temperature (C)": 464,
+   "Surface Pressure (bars)": "92",
+   "Number of Moons": 0,
+   "Ring System?": "No",
+   "Global Magnetic Field?": "No"
+},
+ {
+   "Planet": "Earth",
+   "Color": "Blue, Brown Green and White",
+   "Mass (10^24kg)": 5.97,
+   "Diameter (km)": 12756,
+   "Density (kg": {
+      "m^3)": 5514
+   },
+   "Surface Gravity(m": {
+      "s^2)": 9.8
+   },
+   "Escape Velocity (km": {
+      "s)": 11.2
+   },
+   "Rotation Period (hours)": 23.9,
+   "Length of Day (hours)": 24,
+   "Distance from Sun (10^6 km)": 149.6,
+   "Perihelion (10^6 km)": 147.1,
+   "Aphelion (10^6 km)": 152.1,
+   "Orbital Period (days)": "365.2",
+   "Orbital Velocity (km": {
+      "s)": 29.8
+   },
+   "Orbital Inclination (degrees)": 0,
+   "Orbital Eccentricity": 0.017,
+   "Obliquity to Orbit (degrees)": 23.4,
+   "Mean Temperature (C)": 15,
+   "Surface Pressure (bars)": "1",
+   "Number of Moons": 1,
+   "Ring System?": "No",
+   "Global Magnetic Field?": "Yes"
+},
+ {
+   "Planet": "Mars",
+   "Color": "Red, Brown and Tan",
+   "Mass (10^24kg)": 0.642,
+   "Diameter (km)": 6792,
+   "Density (kg": {
+      "m^3)": 3934
+   },
+   "Surface Gravity(m": {
+      "s^2)": 3.7
+   },
+   "Escape Velocity (km": {
+      "s)": 5
+   },
+   "Rotation Period (hours)": 24.6,
+   "Length of Day (hours)": 24.7,
+   "Distance from Sun (10^6 km)": 228,
+   "Perihelion (10^6 km)": 206.7,
+   "Aphelion (10^6 km)": 249.3,
+   "Orbital Period (days)": "687",
+   "Orbital Velocity (km": {
+      "s)": 24.1
+   },
+   "Orbital Inclination (degrees)": 1.8,
+   "Orbital Eccentricity": 0.094,
+   "Obliquity to Orbit (degrees)": 25.2,
+   "Mean Temperature (C)": -65,
+   "Surface Pressure (bars)": "0.01",
+   "Number of Moons": 2,
+   "Ring System?": "No",
+   "Global Magnetic Field?": "No"
+},
+ {
+   "Planet": "Jupiter",
+   "Color": "Brown, Orange and Tan, with White cloud stripes",
+   "Mass (10^24kg)": 1898,
+   "Diameter (km)": 142984,
+   "Density (kg": {
+      "m^3)": 1326
+   },
+   "Surface Gravity(m": {
+      "s^2)": 23.1
+   },
+   "Escape Velocity (km": {
+      "s)": 59.5
+   },
+   "Rotation Period (hours)": 9.9,
+   "Length of Day (hours)": 9.9,
+   "Distance from Sun (10^6 km)": 778.5,
+   "Perihelion (10^6 km)": 740.6,
+   "Aphelion (10^6 km)": 816.4,
+   "Orbital Period (days)": "4331",
+   "Orbital Velocity (km": {
+      "s)": 13.1
+   },
+   "Orbital Inclination (degrees)": 1.3,
+   "Orbital Eccentricity": 0.049,
+   "Obliquity to Orbit (degrees)": 3.1,
+   "Mean Temperature (C)": -110,
+   "Surface Pressure (bars)": "Unknown",
+   "Number of Moons": 79,
+   "Ring System?": "Yes",
+   "Global Magnetic Field?": "Yes"
+},
+ {
+   "Planet": "Saturn",
+   "Color": "Golden, Brown, and Blue-Grey",
+   "Mass (10^24kg)": 568,
+   "Diameter (km)": 120536,
+   "Density (kg": {
+      "m^3)": 687
+   },
+   "Surface Gravity(m": {
+      "s^2)": 9
+   },
+   "Escape Velocity (km": {
+      "s)": 35.5
+   },
+   "Rotation Period (hours)": 10.7,
+   "Length of Day (hours)": 10.7,
+   "Distance from Sun (10^6 km)": 1432,
+   "Perihelion (10^6 km)": 1357.6,
+   "Aphelion (10^6 km)": 1506.5,
+   "Orbital Period (days)": "10,747",
+   "Orbital Velocity (km": {
+      "s)": 9.7
+   },
+   "Orbital Inclination (degrees)": 2.5,
+   "Orbital Eccentricity": 0.052,
+   "Obliquity to Orbit (degrees)": 26.7,
+   "Mean Temperature (C)": -140,
+   "Surface Pressure (bars)": "Unknown",
+   "Number of Moons": 82,
+   "Ring System?": "Yes",
+   "Global Magnetic Field?": "Yes"
+},
+ {
+   "Planet": "Uranus",
+   "Color": "Blue-Green",
+   "Mass (10^24kg)": 86.8,
+   "Diameter (km)": 51118,
+   "Density (kg": {
+      "m^3)": 1270
+   },
+   "Surface Gravity(m": {
+      "s^2)": 8.7
+   },
+   "Escape Velocity (km": {
+      "s)": 21.3
+   },
+   "Rotation Period (hours)": -17.2,
+   "Length of Day (hours)": 17.2,
+   "Distance from Sun (10^6 km)": 2867,
+   "Perihelion (10^6 km)": 2732.7,
+   "Aphelion (10^6 km)": 3001.4,
+   "Orbital Period (days)": "30,589",
+   "Orbital Velocity (km": {
+      "s)": 6.8
+   },
+   "Orbital Inclination (degrees)": 0.8,
+   "Orbital Eccentricity": 0.047,
+   "Obliquity to Orbit (degrees)": 97.8,
+   "Mean Temperature (C)": -195,
+   "Surface Pressure (bars)": "Unknown",
+   "Number of Moons": 27,
+   "Ring System?": "Yes",
+   "Global Magnetic Field?": "Yes"
+},
+ {
+   "Planet": "Neptune",
+   "Color": "Blue",
+   "Mass (10^24kg)": 102,
+   "Diameter (km)": 49528,
+   "Density (kg": {
+      "m^3)": 1638
+   },
+   "Surface Gravity(m": {
+      "s^2)": 11
+   },
+   "Escape Velocity (km": {
+      "s)": 23.5
+   },
+   "Rotation Period (hours)": 16.1,
+   "Length of Day (hours)": 16.1,
+   "Distance from Sun (10^6 km)": 4515,
+   "Perihelion (10^6 km)": 4471.1,
+   "Aphelion (10^6 km)": 4558.9,
+   "Orbital Period (days)": "59,800",
+   "Orbital Velocity (km": {
+      "s)": 5.4
+   },
+   "Orbital Inclination (degrees)": 1.8,
+   "Orbital Eccentricity": 0.01,
+   "Obliquity to Orbit (degrees)": 28.3,
+   "Mean Temperature (C)": -200,
+   "Surface Pressure (bars)": "Unknown",
+   "Number of Moons": 14,
+   "Ring System?": "Yes",
+   "Global Magnetic Field?": "Yes"
+}
+]
--- asha/src/App.js
+++ asha/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+// Angel was here
 
 function App() {
   return (
