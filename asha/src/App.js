6f9b9e902cc20fe0a800434eb1392d3d15b302b6
Sam Bartram <99421933+SdcBartram@users.noreply.github.com>
2023-06-05 16:29:24 +0100

    images added & some work towards planetDetail page

--- asha/src/App.js
+++ asha/src/App.js
@@ -3,52 +3,71 @@ import HomeInfo from "./components/HomeInfo";
 import PlanetList from "./components/PlanetList";
 import PlanetSelector from "./components/PlanetSelector";
 import PlanetService from "./services/PlanetService";
-import { useState, useEffect} from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./app.css";
 import PlanetDetail from "./components/PlanetDetail";
 
-
 function App() {
+  const [planets, setPlanets] = useState([]);
+  const [onePlanet, setOnePlanet] = useState([]);
+  // const [planetImages, setPlanetImages] = useState([]);
 
-  const [planets, setPlanets] = useState([])
-  const [onePlanet, setOnePlanet] = useState([])
-  const [planetImages, setPlanetImages] = useState([])
-
-  useEffect(()=> {
+  useEffect(() => {
     PlanetService.getPlanets().then((allPlanets) => {
-      setPlanets(allPlanets)
-    })
-  },[])
+      setPlanets(allPlanets);
+    });
+  }, []);
 
-    useEffect (() => {
-      const lowerCasePlanet = onePlanet.Planet
-        // console.log(typeof lowerCasePlanet)
-        getImageNASA(lowerCasePlanet)
-    },[onePlanet])
+  // useEffect(() => {
+  //   const lowerCasePlanet = onePlanet.Planet;
+  //   // console.log(typeof lowerCasePlanet)
+  //   getImageNASA(lowerCasePlanet);
+  // }, [onePlanet]);
 
   const getOnePlanet = (planetId) => {
     // console.log(planetId)
-    PlanetService.getPlanet(planetId)
-    .then(chosenPlanet => setOnePlanet(chosenPlanet))
-  }
+    PlanetService.getPlanet(planetId).then((chosenPlanet) =>
+      setOnePlanet(chosenPlanet)
+    );
+  };
 
-  const getImageNASA = (planet) => {
-    const url = `https://images-api.nasa.gov/search?q=${planet}`
-    fetch(url)
-      .then(res => res.json())
-      .then(planetImg => setPlanetImages(planetImg.collection))
-      // console.log(planetImg)
-}
+  //API for numerous images. Currently not using.
+  // const getImageNASA = (planet) => {
+  //   const url = `https://images-api.nasa.gov/search?q=${planet}`;
+  //   fetch(url)
+  //     .then((res) => res.json())
+  //     .then((planetImg) => setPlanetImages(planetImg.collection));
+  //   // console.log(planetImg)
+  // };
+
+  // chatGPT suggestion for preventing useEffect running twice. Did not work.
+  // const handlePlanetIdChange = (planetId) => {
+  //   // Handle the planetId change in App.js
+  //   console.log("Selected Planet ID:", planetId);
+  //   // Call the functions that depend on the planetId here
+  //   getOnePlanet(planetId);
+  // };
 
   return (
     <Router>
-    <NavBar/>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<HomeInfo/>} />
-        <Route path="/planets" element={<PlanetList planets={planets}/>} />
-        <Route path="/selector" element={<PlanetSelector/>} />
-        <Route path="/planets/:planetId" element={<PlanetDetail onePlanet={onePlanet} getOnePlanet={getOnePlanet} planetImages={planetImages} getImageNASA={getImageNASA} />} />
+        <Route path="/" element={<HomeInfo />} />
+        <Route path="/planets" element={<PlanetList planets={planets} />} />
+        <Route path="/selector" element={<PlanetSelector />} />
+        <Route
+          path="/planets/:planetId"
+          element={
+            <PlanetDetail
+              onePlanet={onePlanet}
+              getOnePlanet={getOnePlanet}
+              // planetImages={planetImages}
+              // getImageNASA={getImageNASA}
+              // onPlanetIdChange={handlePlanetIdChange}
+            />
+          }
+        />
       </Routes>
     </Router>
   );
--- asha/src/components/PlanetDetail.js
+++ asha/src/components/PlanetDetail.js
@@ -1,28 +1,44 @@
-import { useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { useEffect, require } from "react";
+import { useParams } from "react-router-dom";
 
-const PlanetDetail = ({onePlanet, getOnePlanet, planetImages, getImageNASA }) => {
+const PlanetDetail = ({ onePlanet, getOnePlanet }) => {
+  const { planetId } = useParams();
 
-    const { planetId } = useParams()
+  useEffect(() => {
+    // console.log(planetId)
+    getOnePlanet(planetId);
+    // console.log(planetId)
+  }, [planetId]);
 
-    useEffect (() => {
-        // console.log(planetId)
-        getOnePlanet(planetId)
-        console.log(planetId)
-    },[planetId])
+  // useEffect(() => {
+  //     onPlanetIdChange(planetId); // Call the callback function with the planetId
+  //   }, [planetId]);
 
-    // useEffect (() => {
-    //     console.log(onePlanet)
-    //     getImageNASA(onePlanet.Planet)
-    // },[])
+  // useEffect (() => {
+  //     console.log(onePlanet)
+  //     getImageNASA(onePlanet.Planet)
+  // },[])
 
-    return (
-        <>
-        <h1>{onePlanet.Planet}</h1>
-        <img src={`${planetImages.items[0].links[0].href}`}/>
-        {/* {console.log(planetImages.items[0].links[0].href)} */}
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis nisi nec urna ultrices, gravida dictum nulla pretium. Suspendisse pharetra quam vitae lobortis pulvinar. Donec non dictum orci. Proin non bibendum nisl. Donec dapibus eu dui porta vehicula. Aliquam egestas consequat turpis eu tempus. Quisque quis euismod ante, et pellentesque tortor. Quisque eget ullamcorper nibh. Curabitur tempor tellus sed augue vestibulum dignissim. Donec at felis lectus. Quisque tristique faucibus cursus. Mauris varius tellus quis odio posuere bibendum. Donec ornare magna vel turpis fringilla blandit. Nam et turpis non quam hendrerit tincidunt et quis ante. Aenean sed est eu eros dapibus posuere.</p>
-        </>
-    )
-}
-export default PlanetDetail
+  return (
+    <>
+      <h1>{onePlanet.Planet}</h1>
+      <img src={require(`./assets/${onePlanet.Planet}.jpeg`)} />
+      {console.log(`${onePlanet.Planet}.jpeg`)}
+      {/* <img src={`${planetImages.items[0].links[0].href}`}/> */}
+      {/* {console.log(planetImages.items[0].links[0].href)} */}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis
+        nisi nec urna ultrices, gravida dictum nulla pretium. Suspendisse
+        pharetra quam vitae lobortis pulvinar. Donec non dictum orci. Proin non
+        bibendum nisl. Donec dapibus eu dui porta vehicula. Aliquam egestas
+        consequat turpis eu tempus. Quisque quis euismod ante, et pellentesque
+        tortor. Quisque eget ullamcorper nibh. Curabitur tempor tellus sed augue
+        vestibulum dignissim. Donec at felis lectus. Quisque tristique faucibus
+        cursus. Mauris varius tellus quis odio posuere bibendum. Donec ornare
+        magna vel turpis fringilla blandit. Nam et turpis non quam hendrerit
+        tincidunt et quis ante. Aenean sed est eu eros dapibus posuere.
+      </p>
+    </>
+  );
+};
+export default PlanetDetail;
