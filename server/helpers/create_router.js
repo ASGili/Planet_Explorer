859080c7884f66f23020f54d8742ac592ba016b7
Angel <angelgonzc@yahoo.com>
2023-06-05 10:22:43 +0100

    Added getOne of create_router.js

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -17,6 +17,18 @@ const createRouter = function(collection) {
     });
   });
 
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+    .findOne({ _id: ObjectID(id) })
+    .then((doc) => res.json(doc))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
+
   router.post('/', (req, res) => {
     const newData = req.body;
     collection
