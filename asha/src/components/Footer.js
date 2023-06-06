320b115c60be7482e391c851cf4bfdc576b211dd
Hanna <hannadurham3@gmail.com>
2023-06-06 17:48:32 +0100

    Footer added, not super happy with it!

---
+++ asha/src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+
+
+    return (
+        <>
+            <h4 className="footer-element"> &copy; 2023 A(ric)S(am)H(anna)A(ngel). All rights reserved. Dinnae touch.</h4>
+        </>
+    )
+}
+
+export default Footer
