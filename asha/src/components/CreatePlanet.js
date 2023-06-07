2c2491ae31e4e9e813e336a71adfdb4f41bb2378
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-06-07 10:58:01 +0100

    started adding state for form

--- asha/src/components/CreatePlanet.js
+++ asha/src/components/CreatePlanet.js
@@ -3,6 +3,11 @@ import { useState, useEffect } from "react"
 const CreatePlanet = ({createPlanet})=> {
 
     const [formData, setFormData] = useState({})
+    const [value1, setValue1] = useState("")
+    const [value2, setValue2] = useState("")
+    const [value3, setValue3] = useState(false)
+    const [value4, setValue4] = useState(0)
+    const [value5, setValue5] = useState(0)
 
     const handleCreatePlanet = (event) => {
         event.preventDefault()
@@ -13,14 +18,13 @@ const CreatePlanet = ({createPlanet})=> {
             "MeanTemperatureC" : event.target.create4.value,
             "NumberOfMoons" : event.target.create5.value
         })
-        setTimeout(()=> createPlanet(formData),500)
     }
 
     return (
         <form onSubmit={handleCreatePlanet}>
             <ul>
                 <label id="create1">What is your planet named?</label>
-                <input required id="create1"/>
+                <input value={value1} required id="create1"/>
                 <label id="create2"/>What colour is your planet?<label/>
                 <select required id="create2">
                     <option value=""></option>
@@ -37,9 +41,9 @@ const CreatePlanet = ({createPlanet})=> {
                 <input required id="create3" type="radio" name="hasRings" value="true" />
                 <input id="create3" type="radio" name="hasRings" value="false" />
                 <label required id="create4"/>What is the average temperature on your planet?<label/>
-                <input type="number" min="0" max="1000" id="create4" />
+                <input value={value4} type="number" min="0" max="1000" id="create4" />
                 <label required id="create5"/>How many moons does your planet have?<label/>
-                <input type="number" min="0" max="100" id="create5"/>
+                <input value={value5} type="number" min="0" max="100" id="create5"/>
             </ul>
             <input type="submit"/>  
         </form>
