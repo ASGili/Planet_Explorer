ed2f7f0b4625993156acddb7ae155499b177dd80
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-06-07 09:37:58 +0100

    in progress

--- asha/src/components/CreatePlanet.js
+++ asha/src/components/CreatePlanet.js
@@ -1,28 +1,28 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const CreatePlanet = ({createPlanet})=> {
 
-    const [form, setForm] = useState({})
+    const [formData, setFormData] = useState({})
 
     const handleCreatePlanet = (event) => {
         event.preventDefault()
-        setForm({
+        setFormData({
             "Planet": event.target.create1.value,
-            "Color" : ["default", event.target.create2.value],
+            "Color" : ["Default", event.target.create2.value],
             "RingSystem" : event.target.create3.value,
             "MeanTemperatureC" : event.target.create4.value,
             "NumberOfMoons" : event.target.create5.value
         })
-        createPlanet(form)
+        setTimeout(()=> createPlanet(formData),500)
     }
-    
+
     return (
         <form onSubmit={handleCreatePlanet}>
             <ul>
-                <label id="create1">Give your planet a name</label>
-                <input id="create1"/>
+                <label id="create1">What is your planet named?</label>
+                <input required id="create1"/>
                 <label id="create2"/>What colour is your planet?<label/>
-                <select id="create2">
+                <select required id="create2">
                     <option value=""></option>
                     <option value="Red">Red</option>
                     <option value="Green">Green</option>
@@ -34,11 +34,11 @@ const CreatePlanet = ({createPlanet})=> {
                     <option value="Golden">Golden</option>
                 </select>
                 <label id="create3"/>Does your planet have rings?<label/>
-                <input id="create3" type="radio" name="hasRings" value="true" />
+                <input required id="create3" type="radio" name="hasRings" value="true" />
                 <input id="create3" type="radio" name="hasRings" value="false" />
-                <label id="create4"/>What is the average temperature on your planet?<label/>
+                <label required id="create4"/>What is the average temperature on your planet?<label/>
                 <input type="number" min="0" max="1000" id="create4" />
-                <label id="create5"/>How many moons does your planet have?<label/>
+                <label required id="create5"/>How many moons does your planet have?<label/>
                 <input type="number" min="0" max="100" id="create5"/>
             </ul>
             <input type="submit"/>  
